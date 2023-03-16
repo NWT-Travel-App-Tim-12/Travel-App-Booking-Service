550124e245051b08a066bef39c49552fe5b1838c
abegovac2 <abegovac2@etf.unsa.ba>
2023-03-16 22:23:38 +0100

    Model fix up

--- src/main/java/com/app/travel/models/Booking.java
+++ src/main/java/com/app/travel/models/Booking.java
@@ -1,164 +1,66 @@
 package com.app.travel.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
-@Table(name = "booking")
+@Table(name = "bookings")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "serial")
+    @Getter
+    @Setter
     private Integer id;
+    @Getter
+    @Setter
     private String bookingCode;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private String description;
+    @Getter
+    @Setter
     private String regionNote;
+    @Getter
+    @Setter
     private Integer userRef;
+    @Getter
+    @Setter
     private Integer packageRef;
+    @Getter
+    @Setter
     private Integer numberOfDays;
+    @Getter
+    @Setter
     private Integer cost;
+    @Getter
+    @Setter
     private Integer passengerNumber;
+    @Getter
+    @Setter
     private Boolean paid;
+    @Getter
+    @Setter
     private LocalDate startAt;
+    @Getter
+    @Setter
     private LocalDate createdAt;
 
-    public Booking() {
-    }
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "id")
+    @Getter
+    @Setter
+    private List<Itinerary> itineraries;
 
-    public Booking(Integer id, String bookingCode, String name, String description, String regionNote, Integer userRef, Integer packageRef, Integer numberOfDays, Integer cost, Integer passengerNumber, Boolean paid, LocalDate startAt, LocalDate createdAt) {
-        this.id = id;
-        this.bookingCode = bookingCode;
-        this.name = name;
-        this.description = description;
-        this.regionNote = regionNote;
-        this.userRef = userRef;
-        this.packageRef = packageRef;
-        this.numberOfDays = numberOfDays;
-        this.cost = cost;
-        this.passengerNumber = passengerNumber;
-        this.paid = paid;
-        this.startAt = startAt;
-        this.createdAt = createdAt;
-    }
-
-    public Booking(String bookingCode, String name, String description, String regionNote, Integer userRef, Integer packageRef, Integer numberOfDays, Integer cost, Integer passengerNumber, Boolean paid, LocalDate startAt, LocalDate createdAt) {
-        this.bookingCode = bookingCode;
-        this.name = name;
-        this.description = description;
-        this.regionNote = regionNote;
-        this.userRef = userRef;
-        this.packageRef = packageRef;
-        this.numberOfDays = numberOfDays;
-        this.cost = cost;
-        this.passengerNumber = passengerNumber;
-        this.paid = paid;
-        this.startAt = startAt;
-        this.createdAt = createdAt;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getBookingCode() {
-        return bookingCode;
-    }
-
-    public void setBookingCode(String bookingCode) {
-        this.bookingCode = bookingCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getRegionNote() {
-        return regionNote;
-    }
-
-    public void setRegionNote(String regionNote) {
-        this.regionNote = regionNote;
-    }
-
-    public Integer getUserRef() {
-        return userRef;
-    }
-
-    public void setUserRef(Integer userRef) {
-        this.userRef = userRef;
-    }
-
-    public Integer getPackageRef() {
-        return packageRef;
-    }
-
-    public void setPackageRef(Integer packageRef) {
-        this.packageRef = packageRef;
-    }
-
-    public Integer getNumberOfDays() {
-        return numberOfDays;
-    }
-
-    public void setNumberOfDays(Integer numberOfDays) {
-        this.numberOfDays = numberOfDays;
-    }
-
-    public Integer getCost() {
-        return cost;
-    }
-
-    public void setCost(Integer cost) {
-        this.cost = cost;
-    }
-
-    public Integer getPassengerNumber() {
-        return passengerNumber;
-    }
-
-    public void setPassengerNumber(Integer passengerNumber) {
-        this.passengerNumber = passengerNumber;
-    }
-
-    public Boolean getPaid() {
-        return paid;
-    }
-
-    public void setPaid(Boolean paid) {
-        this.paid = paid;
-    }
-
-    public LocalDate getStartAt() {
-        return startAt;
-    }
-
-    public void setStartAt(LocalDate startAt) {
-        this.startAt = startAt;
-    }
-
-    public LocalDate getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDate createdAt) {
-        this.createdAt = createdAt;
-    }
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "id")
+    @Getter
+    @Setter
+    private List<Review> reviews;
 }
--- src/main/java/com/app/travel/models/Review.java
+++ src/main/java/com/app/travel/models/Review.java
@@ -1,74 +1,30 @@
 package com.app.travel.models;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "review")
+@Table(name = "reviews")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "serial")
+    @Getter
+    @Setter
     private Integer id;
-    private Integer bookingId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Getter
+    @Setter
+    private Booking bookingRef;
+    @Getter
+    @Setter
     private String userRef;
+    @Getter
+    @Setter
     private String comment;
+    @Getter
+    @Setter
     private Float rating;
-
-    public Review(Integer id, Integer bookingId, String userRef, String comment, Float rating) {
-        this.id = id;
-        this.bookingId = bookingId;
-        this.userRef = userRef;
-        this.comment = comment;
-        this.rating = rating;
-    }
-
-    public Review(Integer bookingId, String userRef, String comment, Float rating) {
-        this.bookingId = bookingId;
-        this.userRef = userRef;
-        this.comment = comment;
-        this.rating = rating;
-    }
-
-    public Review() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getBookingId() {
-        return bookingId;
-    }
-
-    public void setBookingId(Integer bookingId) {
-        this.bookingId = bookingId;
-    }
-
-    public String getUserRef() {
-        return userRef;
-    }
-
-    public void setUserRef(String userRef) {
-        this.userRef = userRef;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public Float getRating() {
-        return rating;
-    }
-
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
 }
