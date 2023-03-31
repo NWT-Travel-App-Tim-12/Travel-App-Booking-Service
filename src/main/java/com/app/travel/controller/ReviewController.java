9d9ce1057f9dc999242c1432f4ba89979b87ca40
akunalic1 <akunalic1@etf.unsa.ba>
2023-03-31 15:53:05 +0200

    review CRUD methods skeleton added

--- src/main/java/com/app/travel/controller/ReviewController.java
+++ src/main/java/com/app/travel/controller/ReviewController.java
@@ -1,9 +1,19 @@
 package com.app.travel.controller;
 
+import com.app.travel.models.Review;
 import com.app.travel.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/review")
@@ -13,5 +23,24 @@ public class ReviewController {
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
-
+    @GetMapping(path="/all")
+    public List<Review> getAll() {
+        return reviewService.getAll();
+    }
+    @PostMapping("/new")
+    public ResponseEntity<Review> create(@RequestBody Review review) {
+        return ResponseEntity.ok(reviewService.createReview(review));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Review> update(@RequestBody Review review) {
+        return ResponseEntity.ok(reviewService.updateReview(review));
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Review> patch(@RequestBody Review review) {
+        return ResponseEntity.ok(reviewService.updateReview(review));
+    }
+    @DeleteMapping
+    public ResponseEntity<Integer> delete(Integer id) {
+        return ResponseEntity.ok(reviewService.deleteReview(id));
+    }
 }
--- src/main/java/com/app/travel/service/ReviewService.java
+++ src/main/java/com/app/travel/service/ReviewService.java
@@ -1,8 +1,12 @@
 package com.app.travel.service;
 
+import com.app.travel.models.Review;
 import com.app.travel.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
@@ -11,4 +15,17 @@ public class ReviewService {
         this.reviewRepository = reviewRepository;
     }
 
+    public List<Review> getAll() {
+        return reviewRepository.findAll();
+    }
+    public Review createReview(Review review) {
+        return reviewRepository.save(review);
+    }
+    public Integer deleteReview(Integer id) {
+        reviewRepository.deleteById(id);
+        return 200;
+    }
+    public Review updateReview(Review review) {
+        return reviewRepository.save(review);
+    }
 }
