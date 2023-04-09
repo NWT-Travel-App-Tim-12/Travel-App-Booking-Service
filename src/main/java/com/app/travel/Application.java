e31be34bc278b15471ec4b4ed537a0632aac502a
akunalic1 <akunalic1@etf.unsa.ba>
2023-04-09 20:57:36 +0200

    added swagger documentation

--- src/main/java/com/app/travel/Application.java
+++ src/main/java/com/app/travel/Application.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableAutoConfiguration
+@OpenAPIDefinition
 public class Application {
 
 	public static void main(String[] args) {
